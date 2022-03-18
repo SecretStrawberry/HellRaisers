ff74a1db088d44d3363eff6e142d2f923e74b436
SecretStrawberry <surduhori@gmail.com>
2022-03-18 20:32:24 +0100

    Added grid to RightPanel and divs inside it

--- src/components/PanelRight.js
+++ src/components/PanelRight.js
@@ -3,10 +3,27 @@ import React from "react";
 import "../styles/components/PanelRight.scss";
 
 function PanelRight() {
+  const grid = [];
+
+  function makeGrid(cellNumber) {
+    for (let i = 0; i < cellNumber; i++) {
+      grid.push(
+        <div key={i} id={"cell_" + i}>
+          <h3>{"cell_" + i}</h3>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+          magnam quam in, reiciendis dolor id veniam ab facere, dolorem placeat
+          blanditiis ipsum perspiciatis deleniti, aperiam iste esse quos ducimus
+          harum!
+        </div>
+      );
+    }
+    return grid;
+  }
+
   return (
     <div className="PanelRight">
       <h2>Panel Right</h2>
-      <div className="PanelRight_Items"></div>
+      <div className="PanelRight_Items">{makeGrid(6).flat()}</div>
     </div>
   );
 }
