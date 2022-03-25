44602399054e951931bb15f536f3ef94e3c90f75
SecretStrawberry <surduhori@gmail.com>
2022-03-25 11:10:17 +0100

    href link ahhhhhh

--- src/components/NavigationItems.js
+++ src/components/NavigationItems.js
@@ -3,14 +3,14 @@ import React from "react";
 function NavigationItems() {
   return (
     <ul>
-      <a href="#">
+      <a href="/home">
         <li data-title="home">home</li>
       </a>
-      <a href="#">
+      <a href="/hellraisers">
         <li data-title="hellraisers">hellraisers</li>
       </a>
 
-      <a href="#">
+      <a href="/team">
         <li data-title="team">team</li>
       </a>
     </ul>
